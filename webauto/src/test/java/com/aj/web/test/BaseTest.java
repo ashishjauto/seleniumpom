f28daeedaafe6107232842f534f64a021a7391d0
ashishjauto <ashishjaiswalcs@gmail.com>
2025-04-29 10:52:04 +0530

    init

--- webauto/src/test/java/com/aj/web/test/BaseTest.java
+++ webauto/src/test/java/com/aj/web/test/BaseTest.java
@@ -1,4 +1,15 @@
 package com.aj.web.test;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class BaseTest {
+
+    @Test
+    public void init(){
+
+        System.out.println("Test");
+
+        Assert.assertTrue(true);
+    }
 }
